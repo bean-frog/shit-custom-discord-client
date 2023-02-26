17befff917581bd29ccc05f48521e2befee59586
bean-frog <104943861+bean-frog@users.noreply.github.com>
2023-02-26 15:26:42 -0800

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-const fs = require('fs');
-
-contextBridge.exposeInMainWorld('fs', {
-  readFileSync: fs.readFileSync
-});
